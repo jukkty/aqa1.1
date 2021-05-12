cf90cad32a6701165005b0c0f913f556f3e255cd
jukkty <dima.iachimov@gmail.com>
2021-05-12 14:01:05 +0300

    junit4 added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,42 +1,43 @@
 package ru.netology.service;
 
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
 
-import static org.testng.AssertJUnit.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 
 
 public class CashbackHackServiceTest {
-    CashbackHackService service = new CashbackHackService();
     @Test
-    void shouldShowHowMuchExactlyRemainToBonus() {
+    public void shouldShowHowMuchExactlyRemainToBonus() {
+        CashbackHackService service = new CashbackHackService();
         int amount = 900;
 
         int actual = service.remain(amount);
         int expected = 100;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
     @Test
-    void shouldShowHowMuchExactlyRemainToBonus2() {
+    public void shouldShowHowMuchExactlyRemainToBonus2() {
+        CashbackHackService service = new CashbackHackService();
         int amount = 100;
 
         int actual = service.remain(amount);
         int expected = 900;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
     @Test
-    void hereIsMistake() {
+    public void hereIsMistake() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
 
         int actual = service.remain(amount);
         int expected = 0;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
 }
